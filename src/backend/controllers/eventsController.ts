0659d16befc7d5ef8aa35172b30555671d73f56c
Bela Föhrenbacher <bela.foehrenbacher@ibm.com>
2020-12-06 12:35:30 +0100

    getAllEvents() first try

--- src/backend/controllers/eventsController.ts
+++ src/backend/controllers/eventsController.ts
@@ -3,7 +3,22 @@ var connection = require('mongoose');
  * Controller for handling events
  */
 class eventController {
+    /**
+     * Gets all events stored in DB
+     * @param req nothing
+     * @param res 
+     * @returns http status code and error or list of events
+     */
     static async getAllEvents(req, res){
-        
+        connection.query(
+         `db.events.find()`,
+         function(error, results, fields) {
+             if (error) {
+                 res.status(500).json({ error });
+             } else {
+                 res.status(200).json({ results });
+             }
+         }
+        );
     }
 }
