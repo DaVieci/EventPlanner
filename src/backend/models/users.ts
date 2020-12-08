bf388c39dc1ed5956deedcd915994130b3eb9736
Bela Föhrenbacher <bela.foehrenbacher@ibm.com>
2020-12-08 10:27:44 +0100

    prototyping user accounts

---
+++ src/backend/models/users.ts
@@ -0,0 +1,17 @@
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const UserModel = mongoose.model('User', userSchema);
+module.exports = UserModel;
