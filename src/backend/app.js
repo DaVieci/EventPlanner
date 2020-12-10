fbbdc16ba97a031d5d5c0d859a46dfb5648b9399
Bela Föhrenbacher <bela.foehrenbacher@ibm.com>
2020-12-10 18:19:21 +0100

    middleware prep for auth

--- src/backend/app.js
+++ src/backend/app.js
@@ -33,6 +33,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.use((req, res, next) => {
+  console.log('middleware');
+  next();
+});
+
 // user routes
 app.use(userRoutes);
 
