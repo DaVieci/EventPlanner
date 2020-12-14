b3d45ca2a6ab21da09b44eb7a42773b57f1c1e48
Bela Föhrenbacher <bela.foehrenbacher@ibm.com>
2020-12-14 13:33:36 +0100

    Add datepicker and categories filter Add filter button which should load events Add edit and delete button inside cards Delete history route

--- src/frontend/src/app/dashboard/events.component.ts
+++ src/frontend/src/app/dashboard/events.component.ts
@@ -1,23 +1,41 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+import { NbDatepicker, NbDateService, NbRangepickerComponent } from '@nebular/theme';
 
 @Component({
   selector: 'app-events',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
 
   private user_token: String;
-  private bearer_token = ''; 
+  private bearer_token: any;
 
-  private backend_url = 'http://localhost:3000';
+  min: Date;
+  max: Date;
 
+  events: {
+    _id: String,
+    title: String,
+    start: Date,
+    end: Date,
+    body: String,
+    user: String,
+    image: any,
+    category: String,
+    status: String
+  };
+
+  categories: {
+    _id: String,
+    type: String
+  }
 
   constructor(
     private authService: NbAuthService,
-    private http: HttpClient
+    protected dateService: NbDateService<Date>,
   ) {
     this.authService.onTokenChange()
         .subscribe((token: NbAuthJWTToken) => {
@@ -26,32 +44,59 @@ export class EventsComponent implements OnInit {
             this.bearer_token = 'Bearer '+this.user_token;
           }
         });
-
-      var requestOptions = {
-        method: 'GET',
-        headers: {
-          Authorization: this.bearer_token
-        }
-      };
-
-      fetch("/api/events", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => {
-          //ggf http status 403 & 401 verarbeiten
-          console.log('error', error)
-        });
-
+    console.log("CONSTRUCTOR CALL");
+    console.log();
+    //this.min = this.dateService;
+    //this.max = this.dateService.addMonth(this.dateService.today(), 1);
     }
 
   ngOnInit(): void {
-    this.refreshPageOnTransition();
+    console.log("ONINIT CALL");
+    if(!this.refreshPageOnTransition()) {
+      this.getEvents();
+    }
   }
 
-  refreshPageOnTransition(): void {
+  refreshPageOnTransition(): boolean {
     if (!(sessionStorage.getItem("pageTransition")==="false")) {
       sessionStorage.setItem("pageTransition", "false");
       window.location.reload();
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  getEvents(): void {
+    var requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: this.bearer_token
+      }
+    };
+
+    fetch("/api/events", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        //html rendern für event info...
+        console.log(result);
+        this.events = JSON.parse(result);
+      })
+      .catch(error => {
+        //ggf http status 403 & 401 verarbeiten
+        console.log('error', error)
+      });
+  }
+
+  filter_events(): void {
+    
+  }
+
+  edit_event(event_id: String) {
+
+  }
+
+  delete_event(event_id: String) {
+
   }
 }
